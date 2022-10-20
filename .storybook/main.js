bbb37730ba21e47c4678fa5f10cd960224e9d8b8
Saiya <okarl@outlook.com>
2022-10-20 18:58:25 +0800

    chore: makes dev works again

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,28 +1,17 @@
-
-const path = require('path');
-
+const path = require('path')
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../')
-    });
-
-    config.resolve = config.resolve || {}
-    config.resolve.alias = config.resolve.alias || {}
-    Object.assign(config.resolve.alias, {
-      style: path.resolve(__dirname, '../src/style')
-    });
-
-    // Return the altered config
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    '@storybook/preset-scss',
+  ],
+  "framework": "@storybook/react",
+  webpackFinal: async (config) => {
+    config.resolve.alias['style'] = path.join(__dirname, '../src/style');
     return config;
-  }
-};
+  },
+}
